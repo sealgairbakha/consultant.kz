e955de887e3cc3c22332f43657c758a8861909f3
slbaha <bjostar1@gmail.com>
2026-03-06 02:17:58 +0500

    update project

--- shop/services.py
+++ shop/services.py
@@ -1,17 +1,17 @@
 import os
+import httpx
 from django.core.mail import EmailMessage
 from django.conf import settings
 
 
 def send_document_email(order):
-    """Send purchased document to the customer's email."""
     document = order.document
     subject = f'Ваш документ: {document.title}'
     body = (
         f'Здравствуйте!\n\n'
         f'Спасибо за покупку. Ваш заказ #{order.pk} подтверждён.\n\n'
         f'Документ: {document.title}\n'
-        f'Стоимость: {document.price} ₸\n\n'
+        f'Стоимость: {document.price} тенге\n\n'
         f'Файл прикреплён к этому письму.\n\n'
         f'С уважением,\nКоманда Consultantt'
     )
@@ -23,10 +23,10 @@ def send_document_email(order):
         to=[order.email],
     )
 
-    file_path = document.file.path
-    if os.path.exists(file_path):
-        file_name = os.path.basename(file_path)
-        with open(file_path, 'rb') as f:
-            email.attach(file_name, f.read(), 'application/octet-stream')
+    file_url = document.file.url
+    file_name = os.path.basename(file_url.split('?')[0])
+    response = httpx.get(file_url, timeout=30)
+    response.raise_for_status()
+    email.attach(file_name, response.content, 'application/octet-stream')
 
     email.send(fail_silently=False)

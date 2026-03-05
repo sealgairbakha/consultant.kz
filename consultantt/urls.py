ce816c48f812291bde77e37349137b0e94afe6bf
slbaha <bjostar1@gmail.com>
2026-03-05 22:16:57 +0500

    update project

--- consultantt/urls.py
+++ consultantt/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse  # ✅ добавили
+
+def healthz(request):  # ✅ добавили
+    return HttpResponse("ok")
 
 urlpatterns = [
+    path('healthz/', healthz),  # ✅ добавили
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('shop.urls')),
